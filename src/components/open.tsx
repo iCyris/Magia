1d5f2ed04bf6c6d0c5cc9b261ff45dc7f0b82c32
绿生 <yukai.zyk@alibaba-inc.com>
2021-05-26 20:07:11 +0800

    feat: tweaks

--- src/components/open.tsx
+++ src/components/open.tsx
@@ -19,13 +19,19 @@ const Open: React.FC<{
         <div className="chapter-open__line-top"></div>
         <div className="chapter-open__line-bottom"></div>
       </div>
+      <div className="chapter-open__center-ball">
+        <span></span>
+        <span></span>
+      </div>
       <div className="chapter-open__title">
-        <div className="chapter-open__title-group">
-          <span className="title-mo"></span>
-        </div>
-        <div className="chapter-open__title-group">
-          <span className="title-nv"></span>
-          <span className="title-fa"></span>
+        <div className="chapter-open__title-item-box">
+          <div className="chapter-open__title-item">
+            <span className="title-mo"></span>
+          </div>
+          <div className="chapter-open__title-item">
+            <span className="title-nv"></span>
+            <span className="title-fa"></span>
+          </div>
         </div>
         <div className="chapter-open__title-wrap">
           <div className="chapter-open__title-wrap-item"></div>
