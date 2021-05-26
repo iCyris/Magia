aa38e318fd0b002f30c1ba0964909aba30bbf9dc
绿生 <yukai.zyk@alibaba-inc.com>
2021-05-26 20:08:00 +0800

    feat: tweaks

--- src/Magia.tsx
+++ src/Magia.tsx
@@ -10,20 +10,10 @@ function App() {
     setCurrent(current + 1);
   }
 
-  const renderChapter = (currentIndex: number) => {
-    if (currentIndex === 0) {
-      return ChapterOpen;
-    } else if (currentIndex === 1) {
-      return Chapter1;
-    } else {
-      return ChapterOpen;
-    }
-  }
-
   return (
     <div className="magia">
       {
-        current === 0 && <ChapterOpen />
+        current === 0 && <ChapterOpen changeChapter={changeChapter} />
       }
       {
         current === 1 && <Chapter1 />
