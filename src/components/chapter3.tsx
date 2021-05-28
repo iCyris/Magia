326e484a118ffb3db42761e4d40882ce6394871d
iCyris <cyris1015@gmail.com>
2021-05-29 02:02:41 +0800

    feat: tweaks

--- src/components/chapter3.tsx
+++ src/components/chapter3.tsx
@@ -35,9 +35,9 @@ const Chapter3: React.FC<{
         <span /><span /><span /><span />
       </div>
       <div className="chapter3-character-box chapter3-character-box3">
-      <div className="chapter3-character chapter3-character3"></div>
-      <div className="chapter3-character-mask chapter3-character-mask1"></div>
-      <div className="chapter3-character-mask chapter3-character-mask2"></div>
+        <div className="chapter3-character chapter3-character3"></div>
+        <div className="chapter3-character-mask chapter3-character-mask1"></div>
+        <div className="chapter3-character-mask chapter3-character-mask2"></div>
       </div>
 
       {/* part4 */}
@@ -45,20 +45,28 @@ const Chapter3: React.FC<{
         <span /><span /><span /><span />
         <span /><span /><span /><span />
       </div>
-      <div className="chapter3-character-box chapter3-character-box2">
-        
+      <div className="chapter3-character-box chapter3-character-box4">
+        <div className="chapter3-character chapter3-character4"></div>
+        <div className="chapter3-character-mask chapter3-character-mask1"></div>
+        <div className="chapter3-character-mask chapter3-character-mask2"></div>
       </div>
 
-      {/* part2 */}
-      {/* <div className="chapter3-open2">
-        <span /><span /><span /><span />
-        <span /><span /><span /><span />
+      {/* part5 */}
+      <div className="chapter3-open5">
+        <div className="chapter3-open5-group1">
+          <span /><span /><span /><span />
+          <span /><span /><span /><span />
+        </div>
+        <div className="chapter3-open5-group2">
+          <span /><span /><span /><span />
+          <span /><span /><span /><span />
+        </div>
       </div>
-      <div className="chapter3-character-box chapter3-character-box2">
-        
-      </div> */}
-
-
+      <div className="chapter3-character-box chapter3-character-box5">
+        <div className="chapter3-character chapter3-character5"></div>
+        <div className="chapter3-character-mask chapter3-character-mask1"></div>
+        <div className="chapter3-character-mask chapter3-character-mask2"></div>
+      </div>
     </div>
   )
 }
