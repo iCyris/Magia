fa89680ccc7cbfc599c93e5c472e636b3641a653
iCyris <cyris1015@gmail.com>
2021-05-29 01:12:58 +0800

    feat: tweaks

--- src/components/chapter3.tsx
+++ src/components/chapter3.tsx
@@ -5,7 +5,60 @@ const Chapter3: React.FC<{
 }> = ({
   changeChapter
 }) => {
-  return <div className="chapter3"></div>
+  return (
+    <div className="chapter3">
+      {/* part1 */}
+      <div className="chapter3-open1">
+        <span /><span /><span /><span />
+        <span /><span /><span /><span />
+      </div>
+      <div className="chapter3-character-box chapter3-character-box1">
+        <div className="chapter3-character chapter3-character1"></div>
+        <div className="chapter3-character-mask chapter3-character-mask1"></div>
+        <div className="chapter3-character-mask chapter3-character-mask2"></div>
+      </div>
+
+      {/* part2 */}
+      <div className="chapter3-open2">
+        <span /><span /><span /><span />
+        <span /><span /><span /><span />
+      </div>
+      <div className="chapter3-character-box chapter3-character-box2">
+        <div className="chapter3-character chapter3-character2"></div>
+        <div className="chapter3-character-mask chapter3-character-mask1"></div>
+        <div className="chapter3-character-mask chapter3-character-mask2"></div>
+      </div>
+
+      {/* part3 */}
+      <div className="chapter3-open3">
+        <span /><span /><span /><span />
+        <span /><span /><span /><span />
+      </div>
+      <div className="chapter3-character-box chapter3-character-box2">
+        
+      </div>
+
+      {/* part2 */}
+      {/* <div className="chapter3-open2">
+        <span /><span /><span /><span />
+        <span /><span /><span /><span />
+      </div>
+      <div className="chapter3-character-box chapter3-character-box2">
+        
+      </div> */}
+
+      {/* part2 */}
+      {/* <div className="chapter3-open2">
+        <span /><span /><span /><span />
+        <span /><span /><span /><span />
+      </div>
+      <div className="chapter3-character-box chapter3-character-box2">
+        
+      </div> */}
+
+
+    </div>
+  )
 }
 
 export default Chapter3;
