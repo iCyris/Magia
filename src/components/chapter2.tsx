83f834c15b3049795720e885ba42fac65a1c7c81
绿生 <yukai.zyk@alibaba-inc.com>
2021-05-28 20:28:16 +0800

    feat: ch2

--- src/components/chapter2.tsx
+++ src/components/chapter2.tsx
@@ -16,13 +16,24 @@ const Chapter2: React.FC<{
         </div>
         <div className="chapter2__open_word-mask"></div>
       </div>
+      <div className="chapter2__open_word-shine1"></div>
+      <div className="chapter2__open_word-shine2"></div>
       <div className="chapter2__switch-group1">
-        <span /><span /><span /><span /><span />
-        <span /><span /><span /><span /><span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
       </div>
       <div className="chapter2__switch-group2">
+        <span className="chapter2__switch-group2-stone-wave"></span>
         <div className="chapter2__switch-group2-stone"></div>
-        <span className="chapter2__switch-group2-mask chapter2__switch-group2-mask-1"></span>
+        <span className="chapter2__switch-group2-mask chapter2__switch-group2-mask-1" onAnimationEnd={changeChapter}></span>
         <span className="chapter2__switch-group2-mask chapter2__switch-group2-mask-2"></span>
       </div>
     </div>
